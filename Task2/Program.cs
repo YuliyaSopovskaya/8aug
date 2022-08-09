3154f9e191637a6a3ccc84a3851b0be3f903d21c
Yuliya <sopovskaya1@gmail.com>
2022-08-09 12:36:52 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введи 3 числа и мы тебе скажем какое из них больше");
+            int a, b, c;
+            int max;
+            Console.WriteLine("Введи первое число - ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи второе число - ");
+            b = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Введи третье число - ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+Console.WriteLine($"Число {a} максимальное");
+if (b > a) 
+    max = b;
+    Console.WriteLine($"Число {b} максимальное");
+if (c > max)
+    max = c;
+    Console.WriteLine($"Число {c} максимальное");
+
