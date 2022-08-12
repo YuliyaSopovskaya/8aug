ab9e9a1e44a3916a8e319e96daee5e210c73d87c
Yuliya <sopovskaya1@gmail.com>
2022-08-12 19:27:34 +0300

    Dz 13 za 8 august

---
+++ DZ8august13task/Program.cs
@@ -0,0 +1,10 @@
+﻿//введите число 
+//третья цифра числа
+// третьей цифры нет
+
+Console.WriteLine("Третья цифра числа");
+
+int n = 7895454;
+
+Console.WriteLine(n>99 ? n.ToString()[2]:'-');
+Console.ReadKey(true);
