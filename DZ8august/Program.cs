822b62d852bb38c5c94ada1c1400c3bea1f5f710
Yuliya <sopovskaya1@gmail.com>
2022-08-12 19:31:10 +0300

    DZ 10 za 8 august

---
+++ DZ8august/Program.cs
@@ -0,0 +1,12 @@
+﻿//трехзначное число ввести
+//показать вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//10-99 числа
+//78-8
+//12-2
+
+int number = new Random().Next(10,100); //10.99+1
+
+//Console.WriteLine($"Случ число из диапазона 10-99 {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if(firstDigit == secondDigit) Console.WriteLine("Цифры равны");
+else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit}");
