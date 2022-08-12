2fa0fa75d11c4357d82ef56377fb42ead30f40a0
Yuliya <sopovskaya1@gmail.com>
2022-08-12 19:23:47 +0300

    DZ za 8 august

---
+++ DZ8august15task/Program.cs
@@ -0,0 +1,16 @@
+﻿//День недели
+
+Console.WriteLine("Ведите номер дня недели");
+
+int day;
+day=int.Parse(Console.ReadLine());
+
+if (day<=5) 
+{
+Console.WriteLine("Рабочий день");
+}
+
+else
+{
+    Console.WriteLine("Выходной");
+} 
