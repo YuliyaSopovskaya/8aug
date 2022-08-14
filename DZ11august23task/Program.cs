2bb5d67b8ea361a9630077c063a56de823ae6705
Yuliya <sopovskaya1@gmail.com>
2022-08-14 22:05:15 +0300

    DZ11augustTask23

---
+++ DZ11august23task/Program.cs
@@ -0,0 +1,15 @@
+﻿// число (N) 
+// выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 0)
+{
+for (int i = 1; i <= n; i++)
+{
+Console.WriteLine($"{i}\t{i * i * i}");
+}
+}
+else Console.WriteLine("Введено некорректное значение");
