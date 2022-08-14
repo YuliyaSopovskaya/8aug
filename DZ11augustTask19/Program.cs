370a3e643bf371d121a61b58e8b2d27c054516ff
Yuliya <sopovskaya1@gmail.com>
2022-08-14 22:35:05 +0300

    DZ11augTask19

---
+++ DZ11augustTask19/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+
+{
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine((num == 0) || (reverse(num, 0) == num));
+    Console.ReadKey(true);
+}
+ 
+ static int reverse(int num, int acc)
+{
+    while (num > 0) { acc = acc * 10 + num % 10; num /= 10; }
+    return acc;
+}
