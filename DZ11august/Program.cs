d5339c68b402157aa9119d9d83c7cf5a9656fb52
Yuliya <sopovskaya1@gmail.com>
2022-08-14 21:39:46 +0300

    DZ21end

--- DZ11august/Program.cs
+++ DZ11august/Program.cs
@@ -24,6 +24,5 @@ Console.Write("Введите координаты x1: ");
                 Console.Write("Введите координаты z2: ");
                 z2 = Convert.ToDouble(Console.ReadLine());
 
-distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-Console.WriteLine($"Расстояние между двумя точками равно {distance}");
-Console.ReadKey();
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между двумя точками в 3д пространстве равно {distance}");
